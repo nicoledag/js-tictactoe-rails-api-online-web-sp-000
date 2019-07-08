4650092822cded6091667162a0024590fa7d4c40
github username <github email address>
2019-07-08 14:57:41 -0400

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,15 @@
 // Code your JavaScript / jQuery solution here
+
+var turn = 0;
+
+function player() {
+  if (turn % 2 === 0) {
+    return 'X';
+  } else {
+    return 'O';
+  }
+}
+
+function updateState(x, y) {
+  $(`[data-x=${x}][data-y=${y}]`).html(player());
+}
