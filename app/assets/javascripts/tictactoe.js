8e6ea345715805243d746701bff20d1abc0528b9
github username <github email address>
2019-07-09 12:09:44 -0400

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,4 +1,14 @@
 // Code your JavaScript / jQuery solution here
+const WIN_COMBINATIONS = [
+[0,1,2],
+[3,4,5],
+[6,7,8],
+[0,3,6],
+[1,4,7],
+[2,5,8],
+[0,4,8],
+[6,4,2]
+]
 
 var turn = 0;
 
@@ -16,8 +26,27 @@ function updateState(square) {
 }
 
 
-function setMessage(str) {
-  // message.innerHTML = str
+// function setMessage(str) {
+//   message.innerHTML = str
+// }
 
-  $("#message").html("string");
+function setMessage(message) {
+  $("#message").text(message);
+}
+
+function checkWinner() {
+   var winner = false;
+   var board = {}
+
+   // create board
+   $('td').text((index, square) => board[index] = square);
+
+   WIN_COMBINATIONS.forEach(position => {
+     if (board[position[0]] === board[position[1]] && board[position[1]] === board[position[2]]
+     && board[position[0]] !== "") {
+         setMessage(`Player ${board[position[0]]} Won!`)
+         return winner = true;
+       }
+   })
+     return winner;
 }
